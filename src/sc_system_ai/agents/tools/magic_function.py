63e1df894c1b9cbae9ba0b25cd8db1708e33eaea
yukikimoto <ktc2243020002@edu.kyoto-tech.ac.jp>
2024-08-03 03:43:29 +0900

    loggingを再利用時に影響のないように変更

--- src/sc_system_ai/agents/tools/magic_function.py
+++ src/sc_system_ai/agents/tools/magic_function.py
@@ -1,5 +1,5 @@
 import logging
-from sc_system_ai.logging_config import setup_logging
+
 
 from typing import Type
 from langchain.pydantic_v1 import BaseModel, Field
@@ -7,6 +7,7 @@ from langchain_core.tools import BaseTool
 
 
 logger = logging.getLogger(__name__)
+
 
 class MagicFunctionInput(BaseModel):
     input: int = Field(description="Input value for magic function")
@@ -16,17 +17,20 @@ class MagicFunctionTool(BaseTool):
     name = "magic_function_tool"
     description = "Applies a magic function to an input."
     args_schema: Type[BaseModel] = MagicFunctionInput
-    
+
     def _run(
             self,
             input: int,
     ) -> int:
         """use the tool."""
         logger.info(f"Magic Function Toolが次の値で呼び出されました: {input}")
+        logger.debug(f"Magic Function Toolが次の値で呼び出されました: {input}")
         return input + 2
-    
+
 
 magic_function = MagicFunctionTool()
 
 if __name__ == "__main__":
+    from sc_system_ai.logging_config import setup_logging
+    setup_logging()
     print(magic_function.invoke({"input": 3}))
