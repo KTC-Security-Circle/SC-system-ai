e78f6a5afd82082acb9dfc0d125a058f3a39a2fa
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-09-13 14:39:36 +0900

    ダミーエージェントとツールの作成

---
+++ src/sc_system_ai/agents/official_absence_agent.py
@@ -0,0 +1,99 @@
+# ダミーのエージェント
+
+from langchain_openai import AzureChatOpenAI
+
+from sc_system_ai.template.user_prompts import User
+from sc_system_ai.template.ai_settings import llm
+from sc_system_ai.template.agent import Agent
+# from sc_system_ai.agents.tools import magic_function
+from sc_system_ai.agents.tools.submit_official_absence import submit_official_absence
+
+dummy_agent_tools = [
+    # magic_function
+    submit_official_absence
+]
+dummy_agent_info = """あなたの役割は公欠届を提出することです。
+公欠届の提出にはsubmit_official_absence関数を使用してください。
+
+公欠届の提出に必要な情報とフォーマットは以下の通りです。
+- 名前
+    user_infoから取得してください
+    ユーザーの名前のみ
+- 日付
+    MM/DDの形式
+- 欠席する授業のリスト
+    リストにする
+    1限目から6限目まで順に挿入
+    授業がない場合は空文字を挿入
+
+    ["", "授業名/講師名", "", "授業名/講師名", "", ""]
+- 欠席理由
+    欠席理由のみ
+    文章の長さは問いません
+
+これらの情報が不足している場合は、提出してはいけません。
+その場合は、ユーザーに不足している情報を求めてください。
+
+
+全ての情報が揃ったらユーザーに入力内容の確認を取り、肯定的な返答があった場合のみ提出してください。
+ユーザーから修正があった場合は、修正内容に従ってください。
+
+提出後、または途中でユーザーから取り消しの旨があった場合、あなたはそれまでの情報を破棄してください。
+"""
+
+# agentクラスの作成
+
+
+class DummyAgent(Agent):
+    def __init__(
+            self,
+            llm: AzureChatOpenAI = llm,
+            user_info: User = User()
+    ):
+        super().__init__(
+            llm=llm,
+            user_info=user_info
+        )
+        self.assistant_info = dummy_agent_info
+        super().set_assistant_info(self.assistant_info)
+        super().set_tools(dummy_agent_tools)
+
+    def invoke(self, user_input: str):
+        result = super().invoke(user_input)
+
+        if type(result) is dict:
+            new_conversation = [
+                ("human", user_input),
+                ("ai", result["output"])
+            ]
+            self.user_info.conversations.add_conversations_list(new_conversation)
+
+        return result
+
+
+
+if __name__ == "__main__":
+    from sc_system_ai.logging_config import setup_logging
+    setup_logging()
+    # ユーザー情報
+    user_name = "hogehoge"
+    user_major = "fugafuga専攻"
+    history = [
+        ("human", "こんにちは!"),
+        ("ai", "本日はどのようなご用件でしょうか？")
+    ]
+    user_info = User(name=user_name, major=user_major)
+    user_info.conversations.add_conversations_list(history)
+
+    main_agent = DummyAgent(user_info=user_info)
+    main_agent.display_agent_info()
+    # print(main_agent.get_agent_prompt())
+    main_agent.display_agent_prompt()
+    print(main_agent.invoke("公欠届を提出したい"))
+
+    while True:
+        user = input("ユーザー: ")
+        if user == "exit":
+            break
+        print(main_agent.invoke(user))
+
