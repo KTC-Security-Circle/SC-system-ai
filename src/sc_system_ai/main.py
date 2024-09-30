c0bb9f0f8677aae2957d52867f159add44eee923
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-09-30 19:44:30 +0900

    メインの関数の作成

--- src/sc_system_ai/main.py
+++ src/sc_system_ai/main.py
@@ -1,28 +1,48 @@
-from sc_system_ai.template.ai_settings import llm 
-from sc_system_ai.template.user_prompts import UserPrompt, User
+from typing import Literal
 
-# ユーザー情報
-class Chat():
-    def __init__(self, user_name, user_major, conversation, user_info: User = None,):
-        self.user_name = user_name
-        self.user_major = user_major
-        self.conversation = conversation
-        self.user_info = user_info
+from sc_system_ai.template.ai_settings import llm
+from sc_system_ai.template.user_prompts import User
 
+# エージェント
+AGENTS = Literal[
+    "分類",
+    "ダミー",
+]
 
-    def __str__(self):
-        return f"user_name: {self.user_name}, user_major: {self.user_major}, conversation: {self.conversation},"
+def main(
+    user_name: str,
+    user_major: str,
+    conversations: list[tuple[Literal["ai", "human"], str]],
+    message: str,
+    command: AGENTS = "分類",
+):
+    user_info = User(name=user_name, major=user_major)
+    user_info.conversations.add_conversations_list(conversations)
+
+    try:
+        agent = create_agent(command, user_info)
+        resp = agent.invoke(message=message)
+    except Exception as e:
+        raise e
     
-    def create_user_prompt(self):
-        user = UserPrompt(self.user_name, self.user_major)
-        return user
+    return resp
 
-    def create_chat(self):
-        user = UserPrompt(self.user_name, self.user_major)
-        user_prompt = user.create_user_prompt()
-        return user_prompt
-    
 
+def create_agent(
+    agent_name: AGENTS,
+    user_info: User
+):
+    match agent_name:
+        case "分類":
+            from sc_system_ai.agents.classify_agent import ClassifyAgent
+            agent = ClassifyAgent
+        case "ダミー":
+            from src.sc_system_ai.agents.official_absence_agent import DummyAgent
+            agent = DummyAgent
+        case _:
+            raise ValueError(f"エージェントが存在しません: {agent_name}")
+        
+    return agent(llm=llm, user_info=user_info)
 
 
 
@@ -34,8 +54,9 @@ if __name__ == "__main__":
     user_major = "fugafuga専攻"
     conversation = [
         ("human", "こんにちは!"),
-        ("ai", "本日はどのようなご用件でしょうか？"),
-        ("human", "私の名前と専攻は何ですか？"),
+        ("ai", "本日はどのようなご用件でしょうか？")
     ]
-    user = User(user_name, user_major)
-    chat = Chat(user_name, user_major, conversation, user)
+
+    message = "私の名前と専攻は何ですか？"
+    resp = main(user_name, user_major, conversation, message)
+    print(resp)
