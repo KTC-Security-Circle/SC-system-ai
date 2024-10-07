5003b785f7ab6f37c7af81194875747d00251ff2
haruki26 <167726701+haruki26@users.noreply.github.com>
2024-10-08 00:05:19 +0900

    ドックストリングの追加

--- src/sc_system_ai/main.py
+++ src/sc_system_ai/main.py
@@ -1,3 +1,59 @@
+"""
+### パッケージの使用例
+
+以下の情報が必要となります。
+- ユーザー名(str)
+- 専攻(str)
+- 会話履歴(List[Tuple[str, str]])
+
+```python
+# ユーザー情報
+user_name = "hogehoge"
+user_major = "fugafuga専攻"
+conversation = [
+    ("human", "こんにちは!"),
+    ("ai", "本日はどのようなご用件でしょうか？")
+]
+```
+
+
+Chatクラスをインポートし、エージェントを呼び出せます。
+```python
+chat = Chat(
+    user_name=user_name,
+    user_major=user_major,
+    conversation=conversation
+)
+
+message = "私の名前と専攻は何ですか？"
+resp = chat.invoke(message=message)
+```
+
+
+必要なモジュールをインポートして、エージェントを呼び出すこともできます。
+```python
+from sc_system_ai.template.agent import Agent
+from sc_system_ai.template.user_prompts import User
+
+# ユーザー情報を設定
+user_info = User(name=user_name, major=user_major)
+user.conversations.add_conversations_list(conversation)
+
+# エージェントの設定
+agent = Agent(user_info=user_info)
+
+# メッセージを送信
+message = "私の名前と専攻は何ですか？"
+resp = agent.invoke(message)
+```
+
+各エージェントはagentsディレクトリに格納されています。
+```python
+from sc_system_ai.agents.classify_agent import ClassifyAgent
+agent = ClassifyAgent(user_info=user)
+```
+"""
+
 from typing import Type, Literal
 from importlib import import_module
 
@@ -8,6 +64,29 @@ from sc_system_ai.template.user_prompts import User
 AGENT = Literal["classify", "dummy"]
 
 class Chat:
+    """Chatクラス
+    ユーザー情報と会話履歴を保持し、エージェントとのチャットを行うクラス
+
+    Args:
+        user_name (str): ユーザー名
+        user_major (str): 専攻
+        conversation (list[tuple[str, str]], optional): 会話履歴
+
+    Examples:
+        ```python
+        chat = Chat(
+            user_name="hogehoge",
+            user_major="fugafuga専攻",
+            conversation=[
+                ("human", "こんにちは!"),
+                ("ai", "本日はどのようなご用件でしょうか？")
+            ]
+        )
+
+        message = "私の名前と専攻は何ですか？"
+        resp = chat.invoke(message=message)
+        ```
+    """
     def __init__(
         self,
         user_name: str,
@@ -22,6 +101,24 @@ class Chat:
         message: str,
         command: AGENT = "classify"
     ) -> str:
+        """エージェントを呼び出し、チャットを行う関数
+
+        Args:
+            message (str): メッセージ
+            command (AGENT, optional): 呼び出すエージェント。デフォルトでは分類エージェントを呼び出します。
+
+        Returns:
+            str: エージェントからの返答
+
+        コマンドでエージェントを指定して、エージェントを呼び出す場合。
+        ```python
+        resp = chat.invoke(message="私の名前と専攻は何ですか？", command="dummy")
+        ```
+
+        呼び出し可能なエージェント:
+        - classify: 分類エージェント
+        - dummy: ダミーエージェント
+        """
         agent = self._call_agent(command)
         resp = agent.invoke(message)
         return resp["output"] if type(resp) is dict else resp
@@ -35,9 +132,6 @@ class Chat:
             return agent_class(llm=llm, user_info=self.user)  
         except (ModuleNotFoundError, AttributeError):  
             raise ValueError(f"エージェントが見つかりません: {command}")  
-    
-    
-
 
 
 
