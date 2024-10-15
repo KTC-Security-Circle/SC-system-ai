0d5a72de1bca8090bd70fd14507da2ac58037407
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-10-15 13:19:37 +0000

    agentでon_llm_new_tokenの検証

--- studies/streaming_resp.py
+++ studies/streaming_resp.py
@@ -1,10 +1,11 @@
-import os
 from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
 
-from langchain_openai import AzureChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 
+from sc_system_ai.template.ai_settings import llm
 from sc_system_ai.agents.tools.magic_function import magic_function
 from sc_system_ai.template.system_prompt import PromptTemplate
 from sc_system_ai.template.user_prompts import User
@@ -12,35 +13,64 @@ from sc_system_ai.template.user_prompts import User
 load_dotenv()
 
 class StreamingHandler(BaseCallbackHandler):
-    def __init__(self):
+    def __init__(self, queue: Queue):
         super().__init__()
+        self.queue = queue
     
     def on_llm_new_token(self, token, **kwargs):
-        print(f"on_llm_new_token: {token}")
+        if token:
+            self.queue.put(token)
 
-    def on_llm_end(self, response, **kwargs):
-        print(f"on_llm_end: {response}")
+    def on_agent_finish(self, response, **kwargs):
+        print("end")
+        self.queue.put(None)
 
     def on_llm_error(self, error, **kwargs):
-        print(f"on_llm_error: {error}")
-
-llm = AzureChatOpenAI(
-    azure_deployment=os.environ['AZURE_DEPLOYMENT_NAME'],
-    api_version=os.environ['OPENAI_API_VERSION'],
-    streaming=True,
-    callbacks=[StreamingHandler()]
-)
-
-user = User(name="hoge", major="fuga")
-prompt = PromptTemplate(user_info=user)
-tool = [magic_function]
+        self.queue.put(None)
 
 
-agent = create_tool_calling_agent(
-    llm=llm,
-    tools=tool,
-    prompt=prompt.full_prompt
-)
-agent_executor = AgentExecutor(agent=agent, tools=tool, callbacks=[StreamingHandler()])
+def main():
+    queue = Queue()
+    handler = StreamingHandler(queue)
 
-result = agent_executor.invoke({"messages": "magic_functionに3"})
+    llm.streaming=True
+    llm.callbacks=[handler]
+
+    user = User(name="hoge", major="fuga")
+    prompt = PromptTemplate(user_info=user)
+    tool = [magic_function]
+
+    agent = create_tool_calling_agent(
+        llm=llm,
+        tools=tool,
+        prompt=prompt.full_prompt
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tool, callbacks=[handler])
+    def task():
+        result = agent_executor.invoke({
+            "messages": "magic functionに3を入れて"
+        })
+        print(result)
+
+    thread = Thread(target=task)
+    thread.start()
+
+    resp = ""
+
+    try:
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+
+            resp += token
+            if len(resp) >= 5:
+                yield resp
+                resp = ""
+    finally:
+        yield resp
+        if thread and thread.is_alive():
+            thread.join()
+
+for output in main():
+    print(output)
