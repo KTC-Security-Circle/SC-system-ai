33a21c4bfd070b2fc46aba1a3246528cfa4d1f4f
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-26 14:01:37 +0000

    非同期ジェネレータでストリーミング実装
    挙動が怪しいので別の環境で要検証

--- studies/streaming_resp.py
+++ studies/streaming_resp.py
@@ -1,12 +1,14 @@
-from dotenv import load_dotenv
+import asyncio
 from queue import Queue
 from threading import Thread
+from typing import Any, AsyncIterator
 
+from dotenv import load_dotenv
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.agents import create_tool_calling_agent, AgentExecutor
 
-from sc_system_ai.template.ai_settings import llm
 from sc_system_ai.agents.tools.magic_function import magic_function
+from sc_system_ai.template.ai_settings import llm
 from sc_system_ai.template.system_prompt import PromptTemplate
 from sc_system_ai.template.user_prompts import User
 
@@ -16,21 +18,22 @@ class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue: Queue):
         super().__init__()
         self.queue = queue
-    
-    def on_llm_new_token(self, token, **kwargs):
+
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        print(token)
         if token:
             self.queue.put(token)
 
-    def on_agent_finish(self, response, **kwargs):
+    def on_agent_finish(self, finish, **kwargs: Any) -> None:
         print("end")
         self.queue.put(None)
 
-    def on_llm_error(self, error, **kwargs):
+    def on_llm_error(self, **kwargs: Any) -> None:
         self.queue.put(None)
 
 
-def main():
-    queue = Queue()
+async def main() -> AsyncIterator[str]:
+    queue: Queue = Queue()
     handler = StreamingHandler(queue)
 
     llm.streaming=True
@@ -46,9 +49,9 @@ def main():
         prompt=prompt.full_prompt
     )
     agent_executor = AgentExecutor(agent=agent, tools=tool, callbacks=[handler])
-    def task():
+    def task() -> None:
         result = agent_executor.invoke({
-            "messages": "magic functionに3を入れて"
+            "messages": "悲しい歌を歌ってください"
         })
         print(result)
 
@@ -56,21 +59,23 @@ def main():
     thread.start()
 
     resp = ""
+    while True:
+        if queue.empty():
+            continue
+        token = queue.get()
+        if token is None:
+            break
+        resp += token
+        if len(resp) > 1:
+            yield resp
+            resp = ""
+    thread.join()
+    yield resp
 
-    try:
-        while True:
-            token = queue.get()
-            if token is None:
-                break
+async def job() -> None:
+    async for i in main():
+        print(i)
 
-            resp += token
-            if len(resp) >= 5:
-                yield resp
-                resp = ""
-    finally:
-        yield resp
-        if thread and thread.is_alive():
-            thread.join()
+if __name__ == "__main__":
+    asyncio.run(job())
 
-for output in main():
-    print(output)
