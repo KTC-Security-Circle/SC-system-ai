270016147d330692c910c7dfa8a51d10b97a6906
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-27 15:05:45 +0000

    雑談エージェントの作成

---
+++ src/sc_system_ai/agents/small_talk_agent.py
@@ -0,0 +1,47 @@
+from langchain_openai import AzureChatOpenAI
+
+from sc_system_ai.agents.tools import magic_function
+from sc_system_ai.template.agent import Agent
+from sc_system_ai.template.ai_settings import llm
+from sc_system_ai.template.user_prompts import User
+
+main_agent_tools = [magic_function]
+main_agent_info = "あなたの役割はユーザーと雑談を行うことです。"
+
+# agentクラスの作成
+
+
+class SmallTalkAgent(Agent):
+    def __init__(
+            self,
+            llm: AzureChatOpenAI = llm,
+            user_info: User | None = None,
+    ):
+        super().__init__(
+            llm=llm,
+            user_info=user_info if user_info is not None else User(),
+        )
+        self.assistant_info = main_agent_info
+        super().set_assistant_info(self.assistant_info)
+        super().set_tools(main_agent_tools)
+
+
+if __name__ == "__main__":
+    from sc_system_ai.logging_config import setup_logging
+    setup_logging()
+    # ユーザー情報
+    user_name = "hogehoge"
+    user_major = "fugafuga専攻"
+    history = [
+        ("human", "こんにちは!"),
+        ("ai", "本日はどのようなご用件でしょうか？")
+    ]
+    user_info = User(name=user_name, major=user_major)
+    user_info.conversations.add_conversations_list(history)
+
+    agent = SmallTalkAgent(user_info=user_info)
+    agent.display_agent_info()
+    # print(main_agent.get_agent_prompt())
+    agent.display_agent_prompt()
+    print(agent.invoke("magic function に３をいれて"))
+
